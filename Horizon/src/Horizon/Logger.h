ffa69dc50086fdbcac254d32c7187a30ba83d94c
Ceanze <simnylen@gmail.com>
2019-09-23 11:38:20 +0200

    Removed warning C4251

--- Horizon/src/Horizon/Logger.h
+++ Horizon/src/Horizon/Logger.h
@@ -1,7 +1,11 @@
 #pragma once
+#pragma 
 
 #include "Core.h"   
 #include "spdlog/spdlog.h"
+
+#pragma warning(push)
+#pragma warning(disable:4251)
 
 namespace Horizon {
 
@@ -10,8 +14,8 @@ namespace Horizon {
 	public:
 		static void init();
 
-		inline static std::shared_ptr<spdlog::logger>& getCoreLogger() { return coreLogger; }
-		inline static std::shared_ptr<spdlog::logger>& getClientLogger() { return clientLogger; }
+		inline static std::shared_ptr<spdlog::logger> getCoreLogger() { return coreLogger; }
+		inline static std::shared_ptr<spdlog::logger> getClientLogger() { return clientLogger; }
 
 	private:
 		static std::shared_ptr<spdlog::logger> coreLogger;
@@ -24,11 +28,14 @@ namespace Horizon {
 #define HZ_CORE_INFO(...) ::Horizon::Logger::getCoreLogger()->info(__VA_ARGS__)
 #define HZ_CORE_WARN(...) ::Horizon::Logger::getCoreLogger()->warn(__VA_ARGS__)
 #define HZ_CORE_ERROR(...) ::Horizon::Logger::getCoreLogger()->error(__VA_ARGS__)
-#define HZ_CORE_FATAL(...) ::Horizon::Logger::getCoreLogger()->fatal(__VA_ARGS__)
+#define HZ_CORE_FATAL(...) ::Horizon::Logger::getCoreLogger()->critical(__VA_ARGS__)
 
 // Client logger macros
 #define HZ_TRACE(...) ::Horizon::Logger::getClientLogger()->trace(__VA_ARGS__)
 #define HZ_INFO(...) ::Horizon::Logger::getClientLogger()->info(__VA_ARGS__)
 #define HZ_WARN(...) ::Horizon::Logger::getClientLogger()->warn(__VA_ARGS__)
 #define HZ_ERROR(...) ::Horizon::Logger::getClientLogger()->error(__VA_ARGS__)
-#define HZ_FATAL(...) ::Horizon::Logger::getClientLogger()->fatal(__VA_ARGS__)
+#define HZ_FATAL(...) ::Horizon::Logger::getClientLogger()->critical(__VA_ARGS__)
+
+
+#pragma warning(pop)
